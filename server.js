cced921bbc4530648693cda555a4a0802b420b0f
Shreyas Lad <shadowtemplates@gmail.com>
2018-03-26 12:16:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const app = require('express');
+const http = require('http').Server(app);
 
+app.get("/", function(req, res) {
+  res.send('Hello World!');
+});
+
+http.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
