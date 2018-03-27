8171743f5efe6ddb4fe590de18157f9aa7e0f836
Shreyas Lad <shadowtemplates@gmail.com>
2018-03-27 11:39:56 -0700

    user disconnecting

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket) {
   console.log('A user connected');
+  socket.on('disconnect', function(){
+    console.log('A user disconnected');
+  });
 });
 
 http.listen(3000, function() {
