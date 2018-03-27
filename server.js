483174418e936c8ed6e7794068d9fcee3da14492
Shreyas Lad <shadowtemplates@gmail.com>
2018-03-27 11:36:00 -0700

    fixed backend

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const app = require('express');
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get("/", function(req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + './index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('A user connected');
 });
 
 http.listen(3000, function() {
-  console.log("Listening on port 3000");
+  console.log('Listening on *:3000:');
 });
